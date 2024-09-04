5b968ff893c17f91d8d11f9e2320b07dfa1c8348
Tom Jose <32997076+tomkaviyil@users.noreply.github.com>
2024-09-04 16:21:49 +0530

    Update footer.js

--- footer.js
+++ footer.js
@@ -3,7 +3,6 @@ document.getElementById('footer-placeholder').innerHTML = `
 <footer class="footer">
     <div class="footer-container">
         <div class="footer-left">
-            <img src="path_to_your_logo.png" alt="PowerUp Money Logo" class="footer-logo">
             <p>
                 Indiqube Sigma No.3/B, Nexus Koramangala 3rd Block SBI Colony,<br>
                 Koramangala, Bengaluru, Karnataka 560034<br>
