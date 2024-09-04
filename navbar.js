a859fa1e9daed51046a6035a6f8c34188d5fa711
Tom Jose <32997076+tomkaviyil@users.noreply.github.com>
2024-09-04 16:19:21 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,10 +1,12 @@
 // Inject the navbar HTML into the #navbar div
 document.getElementById('navbar').innerHTML = `
 <div class="navbar" id="myNavbar">
-    <a href="#home">Home</a>
-    <a href="#book">Book a Taxi</a>
-    <a href="#contact">Contact Us</a>
-    <a href="#about">About Us</a>
+    <div class="navbar-links">
+        <a href="#home">Home</a>
+        <a href="#book">Book a Taxi</a>
+        <a href="#contact">Contact Us</a>
+        <a href="#about">About Us</a>
+    </div>
     <a href="javascript:void(0);" class="icon" onclick="toggleNavbar()">
         &#9776;
     </a>
