27d6d628a1db6d76364b90cbfa43a3eeaa04f089
Tom Jose <32997076+tomkaviyil@users.noreply.github.com>
2024-09-04 15:35:29 +0530

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,20 @@
+document.getElementById('navbar').innerHTML = `
+<div class="navbar" id="myNavbar">
+    <a href="#home">Home</a>
+    <a href="#book">Book a Taxi</a>
+    <a href="#contact">Contact Us</a>
+    <a href="#about">About Us</a>
+    <a href="javascript:void(0);" class="icon" onclick="toggleNavbar()">
+        &#9776;
+    </a>
+</div>
+`;
+
+function toggleNavbar() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
